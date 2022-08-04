de9a945480d21b66446cd9c4674b4fdf9bea1c38
joaogui1 <joaoguilhermearujo@gmail.com>
2022-08-04 20:11:56 -0300

    new data and capabilities

--- fetch_data.py
+++ fetch_data.py
@@ -1,5 +1,7 @@
 from pyodide.http import pyfetch, FetchResponse
 
+experiments_mapping = ["convs_normalization", "batch_sizes", "convs", "convs_normalization", "depths"]
+
 async def fetch_data(experiment) -> FetchResponse:
     resp = await pyfetch(f"https://raw.githubusercontent.com/joaogui1/viz_veil/main/data/{experiment}.pickle")
 
