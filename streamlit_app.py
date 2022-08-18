12a21a0348c29868f2b01a6f1b30641856813310
joaogui1 <joaoguilhermearujo@gmail.com>
2022-08-18 10:08:15 -0400

    memoization

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,19 @@
 import streamlit as st
 
-from functools import partial
 import pickle
 
-import matplotlib.pyplot as plt
 from plot_data import plot_score_hist, plot
 
-
+@st.experimental_memo
+def plot_agent(ag, hyp, data):
+    fig = plot_score_hist(data[f'{ag}_{hyp}'], bins=4, N=6, figsize=(28, 11))
+    fig.subplots_adjust(hspace=0.85, wspace=0.17)
+    fig2 = plot(data[f'{ag}_{hyp}'])
+    return fig, fig2
 
 st.title("Lifting the Veil")
 # text = st.text_input()
 agents = st.multiselect("Agents", options=["DrQ_eps", "DER"])
-print(agents)
-hyperparameter = st.radio("Hyperparameter", 
-                        options=["Batch Size", 
-                        "Number of Dense Layers", 
-                        "Number of Convolutional Layers",
-                        "Reward Clipping"]
-                        )
 
 experiments_mapping = { "Activation Function": "layer_funct",
                         "Adam's epsilon": "epsilon",
@@ -31,15 +27,13 @@ experiments_mapping = { "Activation Function": "layer_funct",
                         "Number of Dense Layers": "depths",
                         "Reward Clipping": "clip_rewards"
                     }
-
+hyperparameter = st.radio("Hyperparameter", options=experiments_mapping.keys())
 hyp = experiments_mapping[hyperparameter]
 
 with open(f'data/{hyp}.pickle', mode='rb') as f:
     data = pickle.load(f)
-for ag in agents:
-    fig = plot_score_hist(data[f'{ag}_{hyp}'], bins=4, N=6, figsize=(28, 11))
-    fig.subplots_adjust(hspace=0.85, wspace=0.17)
-    st.pyplot(fig)
 
-    fig2 = plot(data[f'{ag}_{hyp}'])
+for ag in agents:
+    fig, fig2 = plot_agent(ag, hyp, data)
+    st.pyplot(fig)
     st.pyplot(fig2)
