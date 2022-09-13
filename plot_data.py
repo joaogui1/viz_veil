35cc76fd42ae9cf229cdb8ab19416fdb82fd8f5f
joaogui1 <joaoguilhermearujo@gmail.com>
2022-09-13 09:37:55 -0300

    plot new data

--- plot_data.py
+++ plot_data.py
@@ -192,3 +192,31 @@ def plot_human_normalized(all_experiments):
       legend=True,
       ax=ax)
   return fig
+
+
+def plot_all_games(df):
+  ylabels = {'returns': 'Return'}
+  envs = set(df['returns'].env)
+
+  num_cols = 4
+  num_rows = 7 
+  col, row = 0, 0
+  fig, axes = plt.subplots(num_rows, num_cols, figsize=(8 * num_cols, 8 * num_rows))
+  for env in envs:
+    for key in ylabels.keys():
+      env_df = df[key]
+      env_df = env_df[env_df.env == env]
+      ax = axes[row, col]
+      sns.lineplot(x='step', y='val', hue='sweep', data=env_df, ax=ax)
+      title = f'{env}'
+      ax.set_title(title, fontsize=22)
+      ax.set_ylabel(ylabels[key], fontsize=18)
+      xlabel = 'Step'
+      ax.set_xlabel(xlabel, fontsize=18)
+      if key == 'losses':
+        ax.set_yscale('symlog')
+      col += 1
+      if col == num_cols:
+        col = 0
+        row += 1
+  return fig
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 import pickle
 
-from plot_data import plot, plot_human_normalized
+from plot_data import plot, plot_human_normalized, plot_all_games
 
 plot = st.experimental_memo(plot)
 plot_human_normalized = st.experimental_memo(plot_human_normalized)
@@ -24,9 +24,13 @@ experiments_mapping = { "Activation Function": "layer_funct",
                         "Number of Atoms": "num_atoms", 
                         "Number of Convolutional Layers": "convs", 
                         "Number of Dense Layers": "depths",
+                        "Replay Capacity": "replay_capacity",
                         "Reward Clipping": "clip_rewards",
+                        "Target Update Period": "target_update_periods",
                         "Update Horizon": "update_horizon",
+                        "Update Period": "update_periods",
                         "Weight Decay": "weightdecay",
+                        "Width": "widths",
                     }
 hyperparameter = st.radio("Hyperparameter", options=experiments_mapping.keys())
 hyp = experiments_mapping[hyperparameter]
@@ -39,6 +43,9 @@ try:
         data2 = pickle.load(f)
 except:
     data2 = None
+
+with open(f'data/curves_all_games/{hyp}.pickle', mode='rb') as f:
+    data3 = pickle.load(f)
 
 col1, col2 = st.columns(2)
 ag_col = {"DrQ_eps": col1, "DER": col2}
@@ -56,3 +63,6 @@ for ag in agents:
     if data2 is not None:
         fig2 = plot_human_normalized(data2[f'{ag}_{hyp}'])
         ag_col[ag].pyplot(fig2)
+
+    fig3 = plot_all_games(data3[f'{ag}_{hyp}'])
+    ag_col[ag].pyplot(fig3)
