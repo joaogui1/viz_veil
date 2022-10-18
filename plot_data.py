a2eb924af30e3923b6f9cd2062e695cdbd3e178b
joaogui1 <joaoguilhermearujo@gmail.com>
2022-10-18 09:30:23 -0300

    remove histogram

--- plot_data.py
+++ plot_data.py
@@ -32,45 +32,6 @@ def decorate_axis(ax, wrect=10, hrect=10, labelsize='large'):
   ax.spines['left'].set_position(('outward', hrect))
   ax.spines['bottom'].set_position(('outward', wrect))
 
-def plot_score_hist(data, bins=20, figsize=(28, 14), 
-                    fontsize='xx-large', N=6, extra_row=1,
-                    names=None):
-  keys = data.keys()
-  data_r = dict()
-  for key in keys:
-    data_r[f'{key}'] = data[key]
-
-  num_tasks = data[list(keys)[0]].shape[1]
-  if names is None:
-    names = ATARI_100K_GAMES
-  N1 = (num_tasks// N) + extra_row
-  #print('N1:', N1)
-  fig, ax = plt.subplots(nrows=N1, ncols=N, figsize=figsize)
-  for i in range(N):
-    for j in range(N1):
-      idx = j * N + i
-      if idx < num_tasks:
-        ax[j, i].set_title(names[idx], fontsize=fontsize)
-        for key in keys:
-          data_r[f'{key}'] = data[f'{key}'][:, idx]
-
-        g = sns.histplot(data_r, bins=bins, ax=ax[j,i], kde=False, legend= False)
-        if i ==1 and j==4:
-          g= sns.histplot(data_r, bins=bins, ax=ax[j,i], kde=False, legend= True)
-          sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
-
-      else:
-        ax[j, i].axis('off')
-      
-      decorate_axis(ax[j, i], wrect=5, hrect=5, labelsize='xx-large')
-      ax[j, i].xaxis.set_major_locator(plt.MaxNLocator(4))
-      if idx % N == 0:
-        ax[j, i].set_ylabel('Count', size=fontsize)
-      else:
-        ax[j, i].yaxis.label.set_visible(False)
-      ax[j, i].grid(axis='y', alpha=0.1)
-  return fig
-
 
 def plot(all_experiments):
   StratifiedBootstrap = rly.StratifiedBootstrap
