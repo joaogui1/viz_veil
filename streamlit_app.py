0eb2356aae446262a82039937cce46d45d57635b
joaogui1 <joaoguilhermearujo@gmail.com>
2022-10-19 12:17:18 -0300

    use precomputed images

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 import os
 import pickle
+import PIL
+PIL.Image.MAX_IMAGE_PIXELS = 900000000
 
 from plot_data import plot, plot_human_normalized, plot_all_games
 
@@ -39,17 +41,22 @@ hyp = experiments_mapping[hyperparameter]
 #     shim = "40M_experiments"
 # else:
 shim = "100k_experiments"
-with open(f'data/{shim}/final_perf/{hyp}.pickle', mode='rb') as f:
-    data = pickle.load(f)
 
-try:
-    with open(f'data/{shim}/human_normalized_curve/{hyp}.pickle', mode='rb') as f:
-        data2 = pickle.load(f)
-except:
-    data2 = None
+fig1_path = f"figures/{shim}/IQM/{hyperparameter}"
+# with open(f'data/{shim}/final_perf/{hyp}.pickle', mode='rb') as f:
+    # data = pickle.load(f)
 
-with open(f'data/{shim}/curves_all_games/{hyp}.pickle', mode='rb') as f:
-    data3 = pickle.load(f)
+fig2_path = f"figures/{shim}/HNS/{hyperparameter}"
+# try:
+#     with open(f'data/{shim}/human_normalized_curve/{hyp}.pickle', mode='rb') as f:
+#         data2 = pickle.load(f)
+# except:
+#     data2 = None
+
+# with open(f'data/{shim}/curves_all_games/{hyp}.pickle', mode='rb') as f:
+#     data3 = pickle.load(f)
+
+fig3_path = f"figures/{shim}/all_games/{hyperparameter}"
 
 col1, col2 = st.columns(2)
 ag_col = {"DrQ_eps": col1, "DER": col2}
@@ -61,14 +68,21 @@ col2.subheader('DER')
 for ag in agents:
     if ag == "DrQ_eps" and hyp == "num_atoms":
         continue
-    fig = plot(data[f'{ag}_{hyp}'])
-    ag_col[ag].pyplot(fig)
+    # fig = plot(data[f'{ag}_{hyp}'])
+    # ag_col[ag].pyplot(fig)
+    ag_col[ag].image(fig1_path+f"/{ag}.png")
 
 
-    if data2 is not None:
-        fig2 = plot_human_normalized(data2[f'{ag}_{hyp}'])
-        ag_col[ag].pyplot(fig2)
+    # if data2 is not None:
+    #     fig2 = plot_human_normalized(data2[f'{ag}_{hyp}'])
+    #     ag_col[ag].pyplot(fig2)
+    try:
+        ag_col[ag].image(fig2_path+f"/{ag}.png")
+    except:
+        pass
     
 
-    fig3 = plot_all_games(data3[f'{ag}_{hyp}'])
-    ag_col[ag].pyplot(fig3)
+    # fig3 = plot_all_games(data3[f'{ag}_{hyp}'])
+    # ag_col[ag].pyplot(fig3)
+    ag_col[ag].image(fig3_path+f"/{ag}.png")
+
