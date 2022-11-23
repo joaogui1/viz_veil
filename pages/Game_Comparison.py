784b092a868e2602250c86d6cbf7e616d6597947
joaogui1 <joaoguilhermearujo@gmail.com>
2022-11-23 16:42:51 -0300

    modernize game comparison

--- pages/Game_Comparison.py
+++ pages/Game_Comparison.py
@@ -7,18 +7,22 @@ plot_game = st.experimental_memo(plot_game)
 st.set_page_config(layout="wide")
 
 st.title("Lifting the Veil")
-agents = st.multiselect("Agents", options=["DrQ_eps", "DER"])
+agents = ["DrQ_eps", "DER"]
 
 game = st.radio("Game", options=ATARI_100K_GAMES)
-
 
 col1, col2 = st.columns(2)
 ag_col = {"DrQ_eps": col1, "DER": col2}
 
-col1.subheader('DrQ Epsilon')
+col1.subheader('DrQ(ε) 100k')
 
-col2.subheader('DER')
+col2.subheader('DER 100k')
+
 if game is not None:
+    main_path = f"figures/100k_experiments/game_comparison/{game}"
     for ag in agents:
-        fig = plot_game(ag, game)
-        ag_col[ag].pyplot(fig)
+        ag_col[ag].image(main_path+f"/{ag}.png")
+
+col1.subheader('DrQ(ε) 40M')
+
+col2.subheader('DER 40M')
