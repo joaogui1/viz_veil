cf47c9ba001114513d585d83fa777a36ba6450e2
joaogui1 <joaoguilhermearujo@gmail.com>
2023-07-07 09:31:35 -0400

    add iqm ranking

--- precompute.py
+++ precompute.py
@@ -1,23 +1,53 @@
 import os
+import pickle
+
+from matplotlib import pyplot as plt
 
 from plot_data import plot_game, plot_hparam, ATARI_100K_GAMES, experiments_mapping
+from utils import kendall_w
 
 
-for scale in ["100k", "40M"]:
-    for ag in ["DrQ_eps", "DER"]:
-        for game in ATARI_100K_GAMES:
-            fig = plot_game(ag, game, scale)
-            save_dir = f"figures/{scale}_experiments/game_comparison/{game}"
-            if not os.path.isdir(save_dir):
-                os.makedirs(save_dir)
-            fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
+# for scale in ["100k", "40M"]:
+#     for ag in ["DrQ_eps", "DER"]:
+#         for game in ATARI_100K_GAMES:
+#             fig = plot_game(ag, game, scale)
+#             save_dir = f"figures/{scale}_experiments/game_comparison/{game}"
+#             if not os.path.isdir(save_dir):
+#                 os.makedirs(save_dir)
+#             fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
 
 
-for scale in ["100k", "40M"]:
-    for ag in ["DrQ_eps", "DER"]:
-        for k,hparam in experiments_mapping.items():
-            fig = plot_hparam(ag, hparam, scale)
-            save_dir = f"figures/{scale}_experiments/hparam_comparison/{hparam}"
-            if not os.path.isdir(save_dir):
-                os.makedirs(save_dir)
-            fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
+# for scale in ["100k", "40M"]:
+#     for ag in ["DrQ_eps", "DER"]:
+#         for k,hparam in experiments_mapping.items():
+#             fig = plot_hparam(ag, hparam, scale)
+#             save_dir = f"figures/{scale}_experiments/hparam_comparison/{hparam}"
+#             if not os.path.isdir(save_dir):
+#                 os.makedirs(save_dir)
+#             fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
+
+drq_w = dict()
+der_w = dict()
+W_dict = {"DrQ_eps" : dict(),
+          "DER": dict()}
+
+for k, hparam in experiments_mapping.items():
+    with open(f'data/40M_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
+        data = pickle.load(f)
+    keys = list(data.keys())
+    for ag, hp_key in zip(["DER", "DrQ_eps"], keys):
+        if ag == "DrQ_eps" and hparam == "num_atoms":
+            continue
+        if hparam == "normalizations":
+            continue
+        print(ag, hp_key)
+        W_dict[ag][k] = kendall_w(data[hp_key])
+
+fig = plt.bar(list(W_dict["DER"].keys()), list(W_dict["DER"].values()))
+plt.xticks(list(W_dict["DER"].keys()), rotation='vertical')
+plt.show()
+# fig = plot_hparam(ag, hparam, scale)
+# save_dir = f"figures/40M_experiments/kendall_w/{hparam}"
+# if not os.path.isdir(save_dir):
+#     os.makedirs(save_dir)
+# fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
---
+++ utils.py
@@ -0,0 +1,67 @@
+import os
+import pickle
+
+import numpy as np
+
+from rliable import library as rly
+from rliable import metrics
+
+from plot_data import ATARI_100K_GAMES
+
+N_GAMES = len(ATARI_100K_GAMES)
+
+def get_int_estimate(data):
+    return rly.get_interval_estimates(
+        data, 
+        lambda x: np.array([metrics.aggregate_iqm(x)]),
+        reps=50
+    )[1]
+
+def flatten_interval_dict(d):
+    # return [(k, v) for k, v in d.items()]
+    return [(k, v[0, 0], v[1, 0]) for k, v in d.items()]
+
+def interval_to_ranking(scores):
+    rank_by_mean = sorted(scores, key=lambda x: -(x[1] + x[2]))
+    ranking = [1]
+    for idx, interval in enumerate(rank_by_mean[1:], start=1):
+        ranking.append(ranking[idx - 1] + (interval[2] < rank_by_mean[idx-1][1]))
+    return list(zip([mr[0] for mr in rank_by_mean], ranking))
+
+def iqm_to_ranking(scores):
+    rank_by_mean = sorted(scores, key=lambda x: -x[1])
+    return list(zip([mr[0] for mr in rank_by_mean], range(len(rank_by_mean))))
+    
+def get_game_rankings(data):
+    transposed_data = {
+        game: 
+            {
+                hp.split('_')[-1]: data[hp][:, idx].reshape(1, -1)
+                for hp in data.keys()
+            } 
+            for idx, game in enumerate(ATARI_100K_GAMES)
+        }
+    game_intervals = {game: 
+                      flatten_interval_dict(
+                        get_int_estimate(transposed_data[game])) 
+                        for game in transposed_data.keys()}
+    
+    return {game: interval_to_ranking(intervals)
+                for game, intervals in game_intervals.items()}
+
+def kendall_w(data):
+    rankings = get_game_rankings(data)
+    total_rankings = {hp: 
+                      sum(rankings[game][idx][1] for game in rankings.keys())
+                      for idx, (hp, _) in enumerate(rankings[ATARI_100K_GAMES[0]])}
+    mean_ranking = np.mean(list(total_rankings.values()))
+    s = sum((mean_ranking - np.array(list(total_rankings.values())))**2)
+    return 12*s/(N_GAMES*N_GAMES*(5**3 - 5))
+
+
+if __name__ == "__main__":
+    with open(f'data/40M_experiments/final_perf/batch_sizes.pickle', mode='rb') as f:
+        data_batch = pickle.load(f)
+    rankings = get_game_rankings(data_batch['DER_batch_sizes'])
+    print("rankings:", rankings['Alien'])
+    print(kendall_w(data_batch['DER_batch_sizes']))
