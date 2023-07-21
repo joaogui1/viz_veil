c059a6efa754ade7b36a9d61ab91388f692de384
joaogui1 <joaoguilhermearujo@gmail.com>
2023-07-21 08:18:20 -0400

    for some reason the intervals all have low = high

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import os
 import pickle
 
@@ -10,23 +11,45 @@ from plot_data import ATARI_100K_GAMES
 
 N_GAMES = len(ATARI_100K_GAMES)
 
+Interval = namedtuple("Interval", "hparam lo hi")
+
 def get_int_estimate(data):
+    reliable = rly.get_interval_estimates(
+        data, 
+        lambda x: np.array([metrics.aggregate_iqm(x)]),
+        reps=50
+    )
+    print(reliable)
     return rly.get_interval_estimates(
         data, 
         lambda x: np.array([metrics.aggregate_iqm(x)]),
         reps=50
     )[1]
 
+def get_iqm_estimate(data):
+    return rly.get_interval_estimates(
+        data, 
+        lambda x: np.array([metrics.aggregate_iqm(x)]),
+        reps=50
+    )[0]
+
 def flatten_interval_dict(d):
-    # return [(k, v) for k, v in d.items()]
-    return [(k, v[0, 0], v[1, 0]) for k, v in d.items()]
+    return [Interval(k, v[0, 0], v[1, 0]) for k, v in d.items()]
+
+def flatten_iqm_dict(d):
+    return [(k, v) for k, v in d.items()]
 
 def interval_to_ranking(scores):
-    rank_by_mean = sorted(scores, key=lambda x: -(x[1] + x[2]))
-    ranking = [1]
-    for idx, interval in enumerate(rank_by_mean[1:], start=1):
-        ranking.append(ranking[idx - 1] + (interval[2] < rank_by_mean[idx-1][1]))
-    return list(zip([mr[0] for mr in rank_by_mean], ranking))
+    rank_by_high = sorted(scores, key=lambda x: -x.hi)
+    rank_by_high.append(Interval(-1, -100, -100)) # Sentinel
+    begin_equal = 0
+    ranks = np.ones((len(scores),))
+    for idx, interval in enumerate(rank_by_high):
+        if interval.hi <= rank_by_high[idx - 1].lo: #statistically different
+            ranks[begin_equal:idx] = (begin_equal + 1 + idx - 1 + 1)/2 #average rank
+            begin_equal = idx
+    import pdb; pdb.set_trace()
+    return list(zip([mr.hparam for mr in rank_by_high], ranks))
 
 def iqm_to_ranking(scores):
     rank_by_mean = sorted(scores, key=lambda x: -x[1])
@@ -45,7 +68,7 @@ def get_game_rankings(data):
                       flatten_interval_dict(
                         get_int_estimate(transposed_data[game])) 
                         for game in transposed_data.keys()}
-    
+    import pdb; pdb.set_trace()
     return {game: interval_to_ranking(intervals)
                 for game, intervals in game_intervals.items()}
 
@@ -62,8 +85,8 @@ def kendall_w(data):
 
 
 if __name__ == "__main__":
-    with open(f'data/40M_experiments/final_perf/batch_sizes.pickle', mode='rb') as f:
-        data_batch = pickle.load(f)
-    rankings = get_game_rankings(data_batch['DER_batch_sizes'])
+    with open(f'data/40M_experiments/final_perf/update_periods.pickle', mode='rb') as f:
+        data = pickle.load(f)
+    rankings = get_game_rankings(data['DER_update_periods'])
     print("rankings:", rankings['Alien'])
-    print(kendall_w(data_batch['DER_batch_sizes']))
+    print(kendall_w(data['DER_update_periods']))
