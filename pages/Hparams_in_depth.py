0dc5a2571e893c092b4f1065bc8013eb907f17e2
joaogui1 <joaoguilhermearujo@gmail.com>
2023-10-24 06:38:07 -0400

    fix this metric

--- pages/Hparams_in_depth.py
+++ pages/Hparams_in_depth.py
@@ -10,7 +10,8 @@ st.set_page_config(layout="wide")
 st.title("Lifting the Veil")
 agents = ["DrQ_eps", "DER"]
 
-hparam = experiments_mapping[st.radio("Hyperparameter", options=list(experiments_mapping.keys()))]
+hparam_name = st.radio("Hyperparameter", options=list(experiments_mapping.keys()))
+hparam = experiments_mapping[hparam_name]
 
 col1, col2 = st.columns(2)
 ag_col = {"DrQ_eps": col1, "DER": col2}
@@ -29,9 +30,11 @@ col1.subheader('DrQ(ε) 40M')
 col2.subheader('DER 40M')
 
 if hparam is not None:
-    col1.subheader(f'DrQ(ε) 40M: {THIS_METRIC["DrQ_eps"][hparam]}')
+    drq_mean, drq_std = THIS_METRIC["DrQ_eps"][hparam_name]
+    col1.subheader(f'THIS Metric: {drq_mean:.2f} ± {drq_std:.2f}')
+    der_mean, der_std = THIS_METRIC["DER"][hparam_name]
+    col2.subheader(f'THIS Metric: {der_mean:.2f} ± {der_std:.2f}')
 
-    col2.subheader(f'DER 40M: {THIS_METRIC["DER"][hparam]}')
     main_path = f"figures/40M_experiments/hparam_comparison/{hparam}"
     for ag in agents:
         ag_col[ag].image(main_path+f"/{ag}.png")
