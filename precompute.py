a96a618e58c9a196330e81ce61ac9674d77fe674
joaogui1 <joaoguilhermearujo@gmail.com>
2023-11-05 16:11:31 -0500

    update game figures

--- precompute.py
+++ precompute.py
@@ -13,14 +13,14 @@ from plot_data import plot_game, plot_hparam, ATARI_100K_GAMES, experiments_mapp
 from utils import get_agent_metric, get_this_metric
 
 
-# for scale in ["100k", "40M"]:
-#     for ag in ["DrQ_eps", "DER"]:
-#         for game in ATARI_100K_GAMES:
-#             fig = plot_game(ag, game, scale)
-#             save_dir = f"figures/{scale}_experiments/game_comparison/{game}"
-#             if not os.path.isdir(save_dir):
-#                 os.makedirs(save_dir)
-#             fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
+for scale in ["100k", "40M"]:
+    for ag in ["DrQ_eps", "DER"]:
+        for game in ATARI_100K_GAMES:
+            fig = plot_game(ag, game, scale)
+            save_dir = f"figures/{scale}_experiments/game_comparison/{game}"
+            if not os.path.isdir(save_dir):
+                os.makedirs(save_dir)
+            fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
 
 
 # for scale in ["100k", "40M"]:
@@ -77,28 +77,28 @@ Get THIS metric between the 2 agents
 # ax.figure.savefig("figures/100k_THIS_agents_aoc.pdf", bbox_inches='tight')
 # ax.figure.savefig("figures/100k_THIS_agents_aoc.png", bbox_inches='tight')
 
-pd_dict = {'Data Regime': [], 'HParam': [], 'Value': []}
+# pd_dict = {'Data Regime': [], 'HParam': [], 'Value': []}
 
-for k, hparam in experiments_mapping.items():
-    data = dict()
-    with open(f'data/100k_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
-        data["100k"] = pickle.load(f)
-    with open(f'data/40M_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
-        data["40M"] = pickle.load(f)
-    keys = list(data["100k"].keys())
-    for data_regime, hp_key in zip(["100k", "40M"], keys):
-        # if ag == "DrQ_eps" and hparam == "num_atoms":
-        #     continue
-        if hparam == "normalizations":
-            continue
-        scores_DER, scores_DrQ = data[data_regime][f"DER_{hparam}"], data[data_regime][f"DrQ_eps_{hparam}"]
-        hparam_val =  get_this_metric(data["100k"][hp_key])
-        pd_dict['Data Regime'].append(data_regime)
-        pd_dict['HParam'].append(k)
-        pd_dict['Value'].append(hparam_val[0])
-df = pd.DataFrame(pd_dict)
-print(df.columns)
-sns.barplot(data=df, x='HParam', y='Value', hue='Algorithm')
-plt.xticks(rotation = 90)
-plt.savefig("figures/100k_experiments/importance_score/THIS_100k_environments.pdf", bbox_inches='tight')
-plt.savefig("figures/100k_experiments/importance_score/THIS_100k_environments.png", bbox_inches='tight')
+# for k, hparam in experiments_mapping.items():
+#     data = dict()
+#     with open(f'data/100k_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
+#         data["100k"] = pickle.load(f)
+#     with open(f'data/40M_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
+#         data["40M"] = pickle.load(f)
+#     keys = list(data["100k"].keys())
+#     for data_regime, hp_key in zip(["100k", "40M"], keys):
+#         # if ag == "DrQ_eps" and hparam == "num_atoms":
+#         #     continue
+#         if hparam == "normalizations":
+#             continue
+#         scores_DER, scores_DrQ = data[data_regime][f"DER_{hparam}"], data[data_regime][f"DrQ_eps_{hparam}"]
+#         hparam_val =  get_this_metric(data["100k"][hp_key])
+#         pd_dict['Data Regime'].append(data_regime)
+#         pd_dict['HParam'].append(k)
+#         pd_dict['Value'].append(hparam_val[0])
+# df = pd.DataFrame(pd_dict)
+# print(df.columns)
+# sns.barplot(data=df, x='HParam', y='Value', hue='Algorithm')
+# plt.xticks(rotation = 90)
+# plt.savefig("figures/100k_experiments/importance_score/THIS_100k_environments.pdf", bbox_inches='tight')
+# plt.savefig("figures/100k_experiments/importance_score/THIS_100k_environments.png", bbox_inches='tight')
