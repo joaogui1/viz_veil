8c5dddb8a139267c2bc32a62f25e424d40a38a50
joaogui1 <joaoguilhermearujo@gmail.com>
2023-11-09 14:36:57 -0500

    fix data regime

--- precompute.py
+++ precompute.py
@@ -13,14 +13,14 @@ from plot_data import plot_game, plot_hparam, ATARI_100K_GAMES, experiments_mapp
 from utils import get_agent_metric, get_this_metric
 
 
-for scale in ["100k", "40M"]:
-    for ag in ["DrQ_eps", "DER"]:
-        for game in ATARI_100K_GAMES:
-            fig = plot_game(ag, game, scale)
-            save_dir = f"figures/{scale}_experiments/game_comparison/{game}"
-            if not os.path.isdir(save_dir):
-                os.makedirs(save_dir)
-            fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
+# for scale in ["100k", "40M"]:
+#     for ag in ["DrQ_eps", "DER"]:
+#         for game in ATARI_100K_GAMES:
+#             fig = plot_game(ag, game, scale)
+#             save_dir = f"figures/{scale}_experiments/game_comparison/{game}"
+#             if not os.path.isdir(save_dir):
+#                 os.makedirs(save_dir)
+#             fig.savefig(f"{save_dir}/{ag}.png", bbox_inches='tight')
 
 
 # for scale in ["100k", "40M"]:
@@ -94,27 +94,31 @@ Get THIS metric between the 2 agents
 #     plt.savefig(f"figures/{data_regime}_experiments/importance_score/THIS_agents.pdf", bbox_inches='tight')
 
 """THIS metric between data regimes"""
-# pd_dict = {'Agent': [], 'HParam': [], 'Value': []}
+pd_dict = {'Agent': [], 'HParam': [], 'Value': []}
 
-# for k, hparam in experiments_mapping.items():
-#     if hparam == "num_atoms":
-#         continue
-#     if hparam == "normalizations":
-#         continue
-#     data = dict()
-#     with open(f'data/100k_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
-#         data["100k"] = pickle.load(f)
-#     with open(f'data/40M_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
-#         data["40M"] = pickle.load(f)
-#     keys = list(data["100k"].keys())
-#     for agent, hp_key in zip(["DrQ_eps", "DER"], keys):
-#         hparam_val =  get_agent_metric(data["100k"][f"{agent}_{hparam}"],
-#                                        data["40M"][f"{agent}_{hparam}"])
-#         pd_dict['Agent'].append(agent)
-#         pd_dict['HParam'].append(k)
-#         pd_dict['Value'].append(hparam_val[0])
-# df = pd.DataFrame(pd_dict)
-# sns.barplot(data=df, x='HParam', y='Value', hue='Agent')
-# plt.xticks(rotation = 90)
-# plt.savefig("figures/split/importance_score/THIS_data_regimes.pdf", bbox_inches='tight')
-# plt.savefig("figures/split/importance_score/THIS_data_regimes.png", bbox_inches='tight')
+for k, hparam in experiments_mapping.items():
+    if hparam == "num_atoms" or hparam == "min_replay_history":
+        continue
+    if hparam == "normalizations" or hparam == "replay_capacity":
+        continue
+    data = dict()
+    with open(f'data/100k_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
+        data["100k"] = pickle.load(f)
+    with open(f'data/40M_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
+        data["40M"] = pickle.load(f)
+    keys = list(data["100k"].keys())
+    for agent, hp_key in zip(["DrQ_eps", "DER"], keys):
+        data["100k"][f"{agent}_{hparam}"] = {
+            k: data["100k"][f"{agent}_{hparam}"][k]
+            for k in data["40M"][f"{agent}_{hparam}"].keys()
+        }
+        hparam_val =  get_agent_metric(data["100k"][f"{agent}_{hparam}"],
+                                       data["40M"][f"{agent}_{hparam}"])
+        pd_dict['Agent'].append(agent)
+        pd_dict['HParam'].append(k)
+        pd_dict['Value'].append(hparam_val[0])
+df = pd.DataFrame(pd_dict)
+sns.barplot(data=df, x='HParam', y='Value', hue='Agent')
+plt.xticks(rotation = 90)
+plt.savefig("figures/split/importance_score/THIS_data_regimes.pdf", bbox_inches='tight')
+plt.savefig("figures/split/importance_score/THIS_data_regimes.png", bbox_inches='tight')
