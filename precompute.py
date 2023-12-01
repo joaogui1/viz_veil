7126a06d4ec1a1259dc63455ebabd2751fbb1539
joaogui1 <joaoguilhermearujo@gmail.com>
2023-12-01 08:12:05 -0500

    computing thc

--- precompute.py
+++ precompute.py
@@ -37,59 +37,65 @@ Get THIS metric between the 26 environments
 """
 
 
-# pd_dict = {'Algorithm': [], 'HParam': [], 'Value': []}
+pd_dict = {'Algorithm': [], 'HParam': [], 'Value': []}
 
-# for k, hparam in experiments_mapping.items():
-#     with open(f'data/100k_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
-#         data = pickle.load(f)
-#     keys = list(data.keys())
-#     for ag, hp_key in zip(["DER", "DrQ_eps"], keys):
-#         if ag == "DrQ_eps" and hparam == "num_atoms":
-#             continue
-#         if hparam == "normalizations":
-#             continue
-#         hparam_val =  get_this_metric(data[hp_key])
-#         pd_dict['Algorithm'].append(ag)
-#         pd_dict['HParam'].append(k)
-#         pd_dict['Value'].append(hparam_val[0])
-# df = pd.DataFrame(pd_dict)
-# print(df.columns)
+for k, hparam in experiments_mapping.items():
+    with open(f'data/40M_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
+        data = pickle.load(f)
+    keys = list(data.keys())
+    for ag, hp_key in zip(["DER", "DrQ_eps"], keys):
+        if ag == "DrQ_eps" and hparam == "num_atoms":
+            continue
+        hparam_val =  get_this_metric(data[hp_key])
+        pd_dict['Algorithm'].append(ag)
+        pd_dict['HParam'].append(k)
+        pd_dict['Value'].append(hparam_val)
+df = pd.DataFrame(pd_dict)
+print(df.columns)
+# print(pd_dict)
+der = df[df["Algorithm"] == 'DER']
+print({row['HParam']: row['Value'] for _, row in der.iterrows()})
+drq = df[df["Algorithm"] == 'DrQ_eps']
+print("\n\n")
+print({row['HParam']: row['Value'] for _, row in drq.iterrows()})
+# print(df[df["Algorithm"] == 'DER'][['HParam', 'Value']].to_dict())
+# print(df[df["Algorithm"] == 'DrQ_eps'][['HParam', 'Value']].to_dict())
 # sns.barplot(data=df, x='HParam', y='Value', hue='Algorithm')
 # plt.xticks(rotation = 90)
-# plt.savefig("figures/100k_experiments/importance_score/THIS_environments.pdf", bbox_inches='tight')
-# plt.savefig("figures/100k_experiments/importance_score/THIS_environments.png", bbox_inches='tight')
+# plt.savefig("figures/40M_experiments/importance_score/THIS_environments.pdf", bbox_inches='tight')
+# plt.savefig("figures/40M_experiments/importance_score/THIS_environments.png", bbox_inches='tight')
 
 """
 Get THIS metric between the 2 agents
 """
 
-pd_dict = {'Data Regime': [], 'HParam': [], 'Value': []}
+# pd_dict = {'Data Regime': [], 'HParam': [], 'Value': []}
 
-for k, hparam in experiments_mapping.items():
-    data = dict()
-    if hparam == "num_atoms":
-            continue
-    with open(f'data/100k_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
-        data["100k"] = pickle.load(f)
-    with open(f'data/40M_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
-        data["40M"] = pickle.load(f)
-    keys = list(data["100k"].keys())
-    for data_regime, hp_key in zip(["100k", "40M"], keys):
-        hparam_val =  get_agent_metric(data[data_regime][f"DrQ_eps_{hparam}"], data[data_regime][f"DER_{hparam}"])
-        pd_dict['Data Regime'].append(data_regime)
-        pd_dict['HParam'].append(k)
-        pd_dict['Value'].append(hparam_val[0])
-df = pd.DataFrame(pd_dict)
-sns.barplot(data=df, x='HParam', y='Value', hue='Data Regime')
-plt.xticks(rotation = 90)
-plt.savefig("figures/split/importance_score/THIS_agents.pdf", bbox_inches='tight')
-plt.savefig("figures/split/importance_score/THIS_agents.png", bbox_inches='tight')
+# for k, hparam in experiments_mapping.items():
+#     data = dict()
+#     if hparam == "num_atoms":
+#             continue
+#     with open(f'data/100k_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
+#         data["100k"] = pickle.load(f)
+#     with open(f'data/40M_experiments/final_perf/{hparam}.pickle', mode='rb') as f:
+#         data["40M"] = pickle.load(f)
+#     keys = list(data["100k"].keys())
+#     for data_regime, hp_key in zip(["100k", "40M"], keys):
+#         hparam_val =  get_agent_metric(data[data_regime][f"DrQ_eps_{hparam}"], data[data_regime][f"DER_{hparam}"])
+#         pd_dict['Data Regime'].append(data_regime)
+#         pd_dict['HParam'].append(k)
+#         pd_dict['Value'].append(hparam_val[0])
+# df = pd.DataFrame(pd_dict)
+# sns.barplot(data=df, x='HParam', y='Value', hue='Data Regime')
+# plt.xticks(rotation = 90)
+# plt.savefig("figures/split/importance_score/THIS_agents.pdf", bbox_inches='tight')
+# plt.savefig("figures/split/importance_score/THIS_agents.png", bbox_inches='tight')
 
-for data_regime in ["100k", "40M"]:
-    sns.barplot(data=df[df['Data Regime'] == data_regime], x='HParam', y='Value')
-    plt.xticks(rotation = 90)
-    plt.savefig(f"figures/{data_regime}_experiments/importance_score/THIS_agents.pdf", bbox_inches='tight')
-    plt.savefig(f"figures/{data_regime}_experiments/importance_score/THIS_agents.pdf", bbox_inches='tight')
+# for data_regime in ["100k", "40M"]:
+#     sns.barplot(data=df[df['Data Regime'] == data_regime], x='HParam', y='Value')
+#     plt.xticks(rotation = 90)
+#     plt.savefig(f"figures/{data_regime}_experiments/importance_score/THIS_agents.pdf", bbox_inches='tight')
+#     plt.savefig(f"figures/{data_regime}_experiments/importance_score/THIS_agents.pdf", bbox_inches='tight')
 
 """THIS metric between data regimes"""
 # pd_dict = {'Agent': [], 'HParam': [], 'Value': []}
